a52c154eebd0736ff8f9ef6d66a84507ded1c699
srisurya6669 <srisurya6669@gmail.com>
2023-11-21 18:34:17 +0530

    update page title

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
