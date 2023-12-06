1041a00caa0d26749337e3c86db0cf6689ccf91a
srisurya6669 <srisurya6669@gmail.com>
2023-12-06 12:17:19 +0530

    simplified time calculations

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
 import genres from "../components/data/genreData";
+import ms from "ms";
 export interface Genre {
   id: number;
   name: string;
@@ -11,7 +12,7 @@ const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms("24h"),
     initialData: genres,
   });
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../components/data/platformData";
 import APIClient, { FetchResponse } from "../services/api-client";
-
+import ms from "ms";
 export interface Platform {
   id: number;
   name: string;
@@ -14,7 +14,7 @@ const usePlatforms = () =>
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
     initialData: platforms,
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms("24h"),
   });
 
 export default usePlatforms;
