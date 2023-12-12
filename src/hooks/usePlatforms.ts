c605f81070291c5f0a09239991048acc72054874
srisurya6669 <srisurya6669@gmail.com>
2023-12-12 15:19:15 +0530

    Moved entity interfaces

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,11 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import platforms from "../components/data/platformData";
 import APIClient, { FetchResponse } from "../services/api-client";
 import ms from "ms";
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+import { Platform } from "../entities/Platform";
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
 const usePlatforms = () =>
