32e7ad12f2f3f3c62e1f27aacff467ea029d34c7
srisurya6669 <srisurya6669@gmail.com>
2023-12-12 19:00:13 +0530

    Added Game Screenshots

---
+++ src/components/gameScreenshots.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import useScreenshots from "../hooks/useScreenShots";
+import { SimpleGrid, Image } from "@chakra-ui/react";
+
+interface Props {
+  gameId: number;
+}
+const gameScreenshots = ({ gameId }: Props) => {
+  const { data, isLoading, error } = useScreenshots(gameId);
+  if (isLoading) return null;
+  if (error) throw error;
+  return (
+    <SimpleGrid spacing={2} columns={{ base: 1, md: 2 }}>
+      {data?.results.map((file) => (
+        <Image key={file.id} src={file.image}></Image>
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default gameScreenshots;
